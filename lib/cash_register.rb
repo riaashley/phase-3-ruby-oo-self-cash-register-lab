448371705775472ca30f70901bebfbed09054a53
Ria Legaspi <riaveluz@gmail.com>
2023-02-02 16:01:09 -0800

    commit tree 19859259ae175d8948505bbb852ad98b5244c92e

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+    attr_accessor :total, :discount, :item
+    def initialize (discount = 0)
+        @total = 0
+        @discount = discount
+        @item = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.previous_total = price * quantity
+        self.total += self.previous_total
+        quanitity.times do
+            self.items << title
+        end
+    end
+
+end
 
